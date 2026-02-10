0efd0e2d3778707805455e0fe143a16557d83506
Vasu-H <195313818+vasu-h10@users.noreply.github.com>
2026-02-10 10:39:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import HookRerender from "./demos/HookRerender";
-// import ParentRerender from "./demos/ParentRerender";
-// import ClassnameChange from "./demos/ClassnameChange";
-// import PureCss from "./demos/PureCss";
 
 export default function App() {
   return <HookRerender />;
